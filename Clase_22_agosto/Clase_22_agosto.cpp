a68345a4256432a3fc48dd839b0b3e994e2cc3d0
Cristopher Madrigal <cristopher.madrigal.chavarria@est.una.ac.cr>
2023-08-22 18:01:26 -0600

    Add initial files of VS

--- Clase_22_agosto/Clase_22_agosto.cpp
+++ Clase_22_agosto/Clase_22_agosto.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+void fillWithRandoms(int*& prices) {
+	prices = new int[50];
+	for (int i = 0; i < 50; i++)
+	{
+		prices[i] = rand() % 90 + 10;
+	}
+}
+float getAveragePrice(int*& prices) {
+	float sum = 0;
+	for (int i = 0; i < 50; i++)
+	{
+		sum += prices[i];
+	}
+	return sum / 50;
+}
+
 int main()
 {
+    int* prices = nullptr;
     std::cout << "Hello World!\n";
 }
 
